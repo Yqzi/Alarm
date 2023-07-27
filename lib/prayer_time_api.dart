bae45dedaf065f467e90356be74fea6ae7fd5617
Yqzi <y.qazi12@gmail.com>
2023-07-26 23:35:52 -0700

    lele

--- lib/prayer_time_api.dart
+++ lib/prayer_time_api.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -7,16 +6,12 @@ String country = "Canada";
 String state = "Alberta";
 String city = "Edmonton";
 String url =
-    "http://api.aladhan.com/v1/calendarByAddress/2017/4?address=$city,$state,$country&method=2";
+    "http://api.aladhan.com/v1/calendarByAddress/2023/7?address=$city,$state,$country&method=2";
 
 Map<String, String> headers = {
   "X-RapidAPI-Key": "8d933572b3mshb10f7c5ca21305ep1433fbjsn10bf498e563b",
   "X-RapidAPI-Host": "aladhan.p.rapidapi.com"
 };
-
-Future<http.Response> getTimes() {
-  return http.get(Uri.parse(url), headers: headers);
-}
 
 class PrayerTiming {
   final String time;
